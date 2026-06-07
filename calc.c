f5aac45bf6a9ae7ef0be1114db81e9e4f13d2d45
exxdi <oleschuiko@gmail.com>
2026-06-07 21:59:47 +0300

    added second function sub()

--- calc.c
+++ calc.c
@@ -3,7 +3,11 @@ double add(double a, double b)
 {
 	return a + b;
 }
-
+double sub(double a, double b)
+{
+	return a - b;
+}
 int main() {
 	printf("%f\n", add(5, 3));
+	printf("%f\n", sub(10, 5));
 }
